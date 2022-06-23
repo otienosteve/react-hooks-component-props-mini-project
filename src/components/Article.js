6093cd8d79bc6ac022ddaf140f5390449a0d8beb
otienosteve <steveotieno701@gmail.com>
2022-06-23 06:15:59 +0300

    dd bentobox emoji

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Article({title,minutes,date="January 1, 1970",preview}) {
  let  coffe='\u2615'
+
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date} {minutes} <span role="img" aria-label="sheep">{coffe.repeat(Math.ceil(minutes/5))}</span></small>
+      <small>{date} {minutes} <span role="img" aria-label="sheep">{minutes<=30?coffe.repeat(Math.ceil(minutes/5)):"🍱".repeat(Math.ceil(minutes/10))}</span></small>
       <p>{preview}</p>
     </article>
   )
